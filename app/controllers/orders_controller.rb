d625e0c1f59562a6936d32f73bee03c7191b44a1
priyanka.alco <priyanka.aleco@gmail.com>
2020-10-16 16:41:49 +0530

    pending button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -174,7 +174,9 @@ class OrdersController < ApplicationController
     get_order_detail = OrderDetail.where("order_id=#{order_id}")
     @result = []
     order = Order.where("id=#{order_id}")
-    last_order =  Order.where("status=#{order[0].order_status}").order("id DESC").last()
+    order_status = order[0].order_status
+    last_order =  Order.where("order_status=#{order_status} and id NOT IN (?) and id < #{order_id.to_i}",[order_id] ).order("id DESC").first();
+
     @next_order = false
     if last_order.present? && last_order.id.to_i != order_id.to_i
       @next_order = true
