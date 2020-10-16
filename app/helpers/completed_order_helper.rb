da8ac6b958a4f70e11e0eeb2ff067137a539e2ea
priyanka.alco <priyanka.aleco@gmail.com>
2020-10-16 15:38:13 +0530

    changes

---
+++ app/helpers/completed_order_helper.rb
@@ -0,0 +1,7 @@
+module CompletedOrderHelper
+  def get_caller_detail(order_id)
+    get_order_detail = Order.where("id=#{order_id}")
+    get_caller_detail = User.where("id=#{get_order_detail[0].seller_id}")
+    return get_caller_detail
+  end
+end
