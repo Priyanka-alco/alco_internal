3d1a44372c5e8e85b61a0c123faa2275290e36e7
priyanka.alco <priyanka.aleco@gmail.com>
2020-10-17 23:32:44 +0530

    gst status

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '*unmatched_route', to: 'application#not_found'
+  # get '*unmatched_route', to: 'application#not_found'
   resources :gsts
   # get '*unmatched_route', to: 'application#not_found'
   resources :discounts
