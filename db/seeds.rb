749d5e7138d444992554c49e824ea1c6bf512ad5
priyanka.alco <priyanka.aleco@gmail.com>
2020-10-21 15:19:53 +0530

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-[{"id"=>1, "first_name"=>"Adit", "last_name"=>"Adit", "email"=>"Adit@alcowhiz.com", "password_digest"=>"123456789", "status"=>true,  "category"=>1}, {"id"=>2, "first_name"=>"Adit", "last_name"=>"Agrawal", "email"=>"manager@gmail.com", "password_digest"=>"123456789", "status"=>true,  "category"=>2}, {"id"=>3, "first_name"=>"Arpit", "last_name"=>"Sharma", "email"=>"packaging@gmail.com", "password_digest"=>"123456789", "status"=>true,  "category"=>3}, {"id"=>5, "first_name"=>"Dileshwar", "last_name"=>"Sahu", "email"=>"dileshwar.sahu@alcoeats.com", "password_digest"=>"20091994", "status"=>true, "category"=>1}, {"id"=>6, "first_name"=>"piyush", "last_name"=>"ramwani", "email"=>"piyush.ramwani@alcoeats.com", "password_digest"=>"piyush@123", "status"=>true, "category"=>1}, {"id"=>7, "first_name"=>"menka", "last_name"=>"sahu", "email"=>"menka.sahu@alcoeats.com", "password_digest"=>"menka11@", "status"=>true, "category"=>1}, {"id"=>8, "first_name"=>"anjita", "last_name"=>"baghel", "email"=>"anjita.baghel@alcoeats.com", "password_digest"=>"8463801190", "status"=>true,  "category"=>1}].each do |aa|
-  User.create!(aa)
+[{"id"=>1, "from_range"=>100, "to_range"=>300, "discount"=>0.0, "status"=>true}, {"id"=>2, "from_range"=>301, "to_range"=>500, "discount"=>5.0, "status"=>true, "created_at"=>Wed, 20 Jan 2010 00:00:00 UTC +00:00, "updated_at"=>Wed, 20 Jan 2010 00:00:00 UTC +00:00}, {"id"=>3, "from_range"=>501, "to_range"=>1000, "discount"=>10.0, "status"=>true}, {"id"=>4, "from_range"=>1001, "to_range"=>2000, "discount"=>15.0, "status"=>true}].each do |aa|
+  Discount.create!(aa)
 end
