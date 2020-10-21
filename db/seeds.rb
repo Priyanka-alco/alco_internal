3119866aae59c73ed11b41083c3806757f2d9904
priyanka.alco <priyanka.aleco@gmail.com>
2020-10-21 15:23:52 +0530

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-[{"id"=>1, "from_range"=>100, "to_range"=>300, "discount"=>0.0, "status"=>true}, {"id"=>2, "from_range"=>301, "to_range"=>500, "discount"=>5.0, "status"=>true, "created_at"=>Wed, 20 Jan 2010 00:00:00 UTC +00:00, "updated_at"=>Wed, 20 Jan 2010 00:00:00 UTC +00:00}, {"id"=>3, "from_range"=>501, "to_range"=>1000, "discount"=>10.0, "status"=>true}, {"id"=>4, "from_range"=>1001, "to_range"=>2000, "discount"=>15.0, "status"=>true}].each do |aa|
+[{"id"=>1, "from_range"=>100, "to_range"=>300, "discount"=>0.0, "status"=>true}, {"id"=>2, "from_range"=>301, "to_range"=>500, "discount"=>5.0, "status"=>true}, {"id"=>3, "from_range"=>501, "to_range"=>1000, "discount"=>10.0, "status"=>true}, {"id"=>4, "from_range"=>1001, "to_range"=>2000, "discount"=>15.0, "status"=>true}].each do |aa|
   Discount.create!(aa)
 end
